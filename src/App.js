a89d9a8c8cbbbf21218ec3f08f116e7386265fe2
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-08-22 22:50:10 +0300

    Refactor App

--- src/App.js
+++ src/App.js
@@ -6,12 +6,35 @@ import PhonesList from "./components/PhonesList";
 import DataProvider from "./components/DataProvider";
 
 function App() {
-  return (
-    <div className="App">
-      <DataProvider getData={() => fetch('users.json').then(res => res.json())}/>
-      <DataProvider getData={() => fetch('phones.json').then(res => res.json())}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <DataProvider getData={() => fetch('users.json').then(res => res.json())}>
+                {value => {
+                    const {error, isFetching, data} = value
+                    if (error) {
+                        return <div>{error.message}</div>
+                    }
+                    if (isFetching) {
+                        return <div>Loading...</div>
+                    }
+                    return <UsersList users={data}/>
+                }}
+            </DataProvider>
+            <DataProvider getData={() => fetch('phones.json').then(res => res.json())}>
+
+                {value => {
+                    const {error, isFetching, data} = value
+                    if (error) {
+                        return <div>{error.message}</div>
+                    }
+                    if (isFetching) {
+                        return <div>Loading...</div>
+                    }
+                    return <PhonesList phones={data}/>
+                }}
+            </DataProvider>
+        </div>
+    );
 }
 
 export default App;
